1cc82b6162577b038584c297ac7eff7c13c67fd1
Stas Gavrilov <89335018+StasGavrilov@users.noreply.github.com>
2022-03-10 03:30:05 +0200

    refresh

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Todos from "./Todos/components/Todos";
-import todoApp from './Todos/css/app.css'
+import app from './Todos/css/app.css'
 
 function App() {
   return (
